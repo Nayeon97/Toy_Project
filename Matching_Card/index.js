34cd73ff34d4e4888747b1d1ddb766818457251a
Naayeon <naaay097@gmail.com>
2022-02-09 15:57:27 +0900

    update file

---
+++ Matching_Card/index.js
@@ -0,0 +1,19 @@
+
+let container = document.querySelector(".container");
+
+class Cards {
+    constructor(card_front, card_back, isfront) {
+        this.card_front = card_front;
+        this.card_back = card_back;
+        this.isfront = isfront;
+    }
+
+}
+
+let array = ['🍕','🍕','🍩','🍩','🌭','🌭','🍔','🍔','🍿','🍿','🥨','🥨','🍟','🍟','🍗','🍗'];
+let cards = [];
+function make(){
+    for(let i=0; i<16; i++){
+        
+    }
+}
