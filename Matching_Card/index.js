2f2e7e48a3680fbe0061ee757e74bba0f22cedfc
Naayeon <naaay097@gmail.com>
2022-02-10 22:56:09 +0900

    update js

--- Matching_Card/index.js
+++ Matching_Card/index.js
@@ -1,3 +1,11 @@
+let selectedCard = document.getElementsByClassName("card");
+let cardB = document.getElementsByClassName("card_back");
+let cardF = document.getElementsByClassName("card_front"); 
+let iconAdd = document.getElementsByClassName("counter")[0];
+let A=[]; // card.isOpen
+let userSelect = [];
+let sucessCnt=0; //성공횟수
+
 function getJson(){
     return fetch('/data.json')
     .then(response=>response.json())
@@ -10,7 +18,6 @@ getJson()
     openArray(cards);
     displayCards(cards);
     setclickEvent();
-
 })
 .catch(console.log)
 
@@ -36,27 +43,57 @@ function Make(card){
  `;
 }
 
-let selectedCard = document.getElementsByClassName("card");
-let cardB = document.getElementsByClassName("card_back");
-let cardF = document.getElementsByClassName("card_front"); //
-
-
-function openArray(cards){
-   a = cards.map(card => card.isOpen);
-   return a;
+function openArray(cards){ // card.isOpen 값들만
+  A = cards.map(card => card.isOpen);
+   return A;
 } // isOpen 배열로 받아옴.
 
-function cardOpen(i){
-    if (a[i] == "close"){
-       console.log(a[i]);
+let num =[];
+function cardOpen(index){
+    if (A[index] == "close"){
+        cardB[index].classList.add("turn");
+        cardF[index].classList.add("turn");
+        A[index]="open"; //isOpen 값 바꿈
+        num.push(index);
+        userSelect.push(cardF[index]);
     }
-}
+ }
+
+ function compare(index){ // 선택한 카드 두개 비교하기
+    if(userSelect[0].firstElementChild.innerHTML == 
+       userSelect[1].firstElementChild.innerHTML){    
+       A[num[0]] ="sucess";
+       A[num[1]] ="sucess";
+       userSelect.length=0;
+       num.length=0;
+       sucessCnt++;
+       console.log(index);
+       iconAdd.innerHTML += `<span>🍴</span>`;
+   }
+   else{
+       for(let i=0; i<A.length; i++){
+           if(A[i] == "open"){
+               cardB[i].classList.remove("turn");
+               cardF[i].classList.remove("turn");
+               A[i]="close";
+           }
+       }
+       userSelect.length=0;
+       num.length=0;
+      }
+   }
 
 function setclickEvent() {
    [...selectedCard].forEach((target ,index) => {
     target.addEventListener("click", (e) => {
-        let i = index;
-        cardOpen(i);
+        if(userSelect.length<2){ // 카드 두개를 선택하지 않았을 때
+            cardOpen(index);
+        }
+        setTimeout(()=>{
+            if(userSelect.length==2){
+                compare(index, index);
+            }
+        },2000)
     })
    });
 }
