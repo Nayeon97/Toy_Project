38c88cfd477f23f78c4d98a9fd6a35235bf228f3
Nayeon097 <naaay097@gmail.com>
2022-02-17 02:47:38 +0900

    FEAT: 두개의 숫자와 기호 구분

--- Calculator/index.js
+++ Calculator/index.js
@@ -7,9 +7,6 @@ const number = document.getElementsByClassName('num'); // 0-9까지 숫자
 const oper = document.getElementsByClassName('operator'); // 연산
 const btnAll = document.querySelectorAll('button');
 
-let num1 = [];
-let num2 = [];
-
 btnAll.forEach(button => {
   button.addEventListener('click', (btn) => {
     switch(btn.target.className || btn.target.id){
@@ -20,14 +17,57 @@ btnAll.forEach(button => {
         display.innerText += btn.target.textContent;
         break;
       case "clear":
-        display.innerText = "";
+        display.innerText = null;
         break;
       case "end":
         display.innerText += i;
           break;
       case "num_decimal":
-        display.innerText += i;
+        display.innerText += btn.target.textContent;
           break;       
     }
   })
 });
+
+let array = [];
+let num_array = [];
+let operator_array = [];
+let n = [];
+let finish;
+
+function Calculate_array(){
+   for(let i=0; i<display.innerText.length; i++){
+      array.push(display.innerText[i]);
+   }
+   return array;
+}
+
+function Calculate_result(){
+  array.forEach((item, index) => {
+    if(isNaN(item) == false || item === ".") {
+			 num_array.push(item);
+       if(index == array.length-1){
+        console.log(index);
+        n.push(parseFloat(num_array.join('')));
+       }
+		} 
+    else {
+      n.push(parseFloat(num_array.join('')));
+      num_array=[];
+      operator_array.push(item);
+		}    
+  });
+}
+
+function aaa(){
+   if(operator_array[0] == "+"){
+     finish = n[0]+n[1];
+     console.log(finish);
+     n=[];
+     operator_array=[];
+   }
+}
+
+
+  
+
