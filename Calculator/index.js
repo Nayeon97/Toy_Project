7923d6bbfe4416d0cfefc7fee25b2380ffb2f548
Nayeon097 <naaay097@gmail.com>
2022-02-18 23:16:58 +0900

    계산기능추가

--- Calculator/index.js
+++ Calculator/index.js
@@ -2,7 +2,8 @@
 
 const result = document.getElementById('end').innerHTML; // =
 const decimal_point = document.getElementById('num_decimal').innerHTML; // 소수점
-const display = document.getElementById('calculator_display');
+const display = document.getElementsByClassName('now_result_text')[0]; // 현재 계산
+const before_result = document.getElementsByClassName('before_result_text')[0]; // 이전 계산
 const number = document.getElementsByClassName('num'); // 0-9까지 숫자
 const oper = document.getElementsByClassName('operator'); // 연산
 const btnAll = document.querySelectorAll('button');
@@ -11,16 +12,27 @@ btnAll.forEach(button => {
   button.addEventListener('click', (btn) => {
     switch(btn.target.className || btn.target.id){
       case "num":
+        if(finish.length==1){
+          before_result.innerText = `Ans = ${finish[0]}`;
+          display.innerText=null;
+          finish.length = 0;
+        }
         display.innerText += btn.target.textContent;
         break;
       case "operator":
+        if(finish.length==1){
+          finish.length = 0;
+        }
         display.innerText += btn.target.textContent;
         break;
       case "clear":
         display.innerText = null;
+        before_result.innerText = null;
         break;
       case "end":
-        display.innerText += i;
+          Calculate_array();
+          Calculate_result();
+          input_display();
           break;
       case "num_decimal":
         display.innerText += btn.target.textContent;
@@ -32,8 +44,8 @@ btnAll.forEach(button => {
 let array = [];
 let num_array = [];
 let operator_array = [];
-let n = [];
-let finish;
+let nums = [];
+let finish = [];
 
 function Calculate_array(){
    for(let i=0; i<display.innerText.length; i++){
@@ -47,25 +59,52 @@ function Calculate_result(){
     if(isNaN(item) == false || item === ".") {
 			 num_array.push(item);
        if(index == array.length-1){
-        console.log(index);
-        n.push(parseFloat(num_array.join('')));
+        nums.push(parseFloat(num_array.join('')));
        }
 		} 
     else {
-      n.push(parseFloat(num_array.join('')));
+      nums.push(parseFloat(num_array.join('')));
       num_array=[];
       operator_array.push(item);
 		}    
   });
 }
 
-function aaa(){
+function input_display(){
    if(operator_array[0] == "+"){
-     finish = n[0]+n[1];
-     console.log(finish);
-     n=[];
-     operator_array=[];
+     finish.push(nums[0]+nums[1]);
+     display.innerText = finish[0];
+     array.length=0;
+     num_array.length=0;
+     nums.length=0;
+     operator_array.length=0;
    }
+   if(operator_array[0] == "-"){
+    finish.push(nums[0]-nums[1]);
+    display.innerText = finish[0];
+    array.length=0;
+    num_array.length=0;
+    nums.length=0;
+    operator_array.length=0;
+  }
+  if(operator_array[0] == "×"){
+    finish.push(nums[0]*nums[1]);
+    display.innerText = finish[0];
+    array.length=0;
+    num_array.length=0;
+    nums.length=0;
+    operator_array.length=0;
+  }
+  if(operator_array[0] == "÷"){
+    console.log(nums[0]);
+    console.log(nums[1]);
+    finish.push(nums[0]/nums[1]);
+    display.innerText = finish[0];
+    array.length=0;
+    num_array.length=0;
+    nums.length=0;
+    operator_array.length=0;
+  }
 }
 
 
