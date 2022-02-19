9ac93e9be434f154635cd613b7766b0616936f56
Nayeon097 <naaay097@gmail.com>
2022-02-19 21:17:34 +0900

    첫번째 입력이 음수인 경우 NAN 으로 출력되는 오류 수정

--- Calculator/index.js
+++ Calculator/index.js
@@ -11,7 +11,7 @@ const btnAll = document.querySelectorAll('button');
 btnAll.forEach(button => {
   button.addEventListener('click', (btn) => {
     switch(btn.target.className || btn.target.id){
-      case "num":
+      case "num": // 숫자
         if(finish.length==1){
           before_result.innerText = `Ans = ${finish[0]}`;
           display.innerText=null;
@@ -19,15 +19,17 @@ btnAll.forEach(button => {
         }
         display.innerText += btn.target.textContent;
         break;
-      case "operator":
+      case "operator": // 연산자
         if(finish.length==1){
           finish.length = 0;
         }
         display.innerText += btn.target.textContent;
         break;
-      case "clear":
+      case "clear": // 끝
         display.innerText = null;
-        before_result.innerText = null;
+        before_result.innerText = null;  
+        finish.length = 0;
+        reset();
         break;
       case "end":
           Calculate_array();
@@ -41,11 +43,12 @@ btnAll.forEach(button => {
   })
 });
 
-let array = [];
-let num_array = [];
+let array = []; 
+let num_array = []; 
 let operator_array = [];
 let nums = [];
 let finish = [];
+let negative_num = [];
 
 function Calculate_array(){
    for(let i=0; i<display.innerText.length; i++){
@@ -56,57 +59,77 @@ function Calculate_array(){
 
 function Calculate_result(){
   array.forEach((item, index) => {
-    if(isNaN(item) == false || item === ".") {
-			 num_array.push(item);
-       if(index == array.length-1){
+    if (index == 0 && item == "-")
+    {
+      negative_num.push(item);
+    }
+    else{
+      if(isNaN(item) == false || item === ".")
+      {
+         num_array.push(item);
+         if(index == array.length-1){
+          nums.push(parseFloat(num_array.join('')));
+         }
+      } 
+      else
+      {
         nums.push(parseFloat(num_array.join('')));
-       }
-		} 
-    else {
-      nums.push(parseFloat(num_array.join('')));
-      num_array=[];
-      operator_array.push(item);
-		}    
+        num_array=[];
+        operator_array.push(item);
+      }    
+    }
   });
 }
 
 function input_display(){
-   if(operator_array[0] == "+"){
-     finish.push(nums[0]+nums[1]);
+  if(negative_num.length == 1){
+    console.log(operator_array[0]);
+    if(operator_array[0] == "+"){
+      finish.push((-1 * nums[0])+nums[1]);
+      display.innerText = finish[0];
+      reset();
+    }
+    if(operator_array[0] == "-"){
+     finish.push((-1 * nums[0])-nums[1]);
      display.innerText = finish[0];
-     array.length=0;
-     num_array.length=0;
-     nums.length=0;
-     operator_array.length=0;
+     reset();
    }
-   if(operator_array[0] == "-"){
-    finish.push(nums[0]-nums[1]);
-    display.innerText = finish[0];
-    array.length=0;
-    num_array.length=0;
-    nums.length=0;
-    operator_array.length=0;
-  }
-  if(operator_array[0] == "×"){
-    finish.push(nums[0]*nums[1]);
-    display.innerText = finish[0];
-    array.length=0;
-    num_array.length=0;
-    nums.length=0;
-    operator_array.length=0;
-  }
-  if(operator_array[0] == "÷"){
-    console.log(nums[0]);
-    console.log(nums[1]);
-    finish.push(nums[0]/nums[1]);
-    display.innerText = finish[0];
-    array.length=0;
-    num_array.length=0;
-    nums.length=0;
-    operator_array.length=0;
-  }
+   if(operator_array[0] == "×"){
+     finish.push((-1 * nums[0])*nums[1]);
+     display.innerText = finish[0];
+     reset();
+   }
+  } 
+  else{
+    if(operator_array[0] == "+"){
+      finish.push(nums[0]+nums[1]);
+      display.innerText = finish[0];
+      reset();
+    }
+    if(operator_array[0] == "-"){
+     finish.push(nums[0]-nums[1]);
+     display.innerText = finish[0];
+     reset();
+   }
+   if(operator_array[0] == "×"){
+     finish.push(nums[0]*nums[1]);
+     display.innerText = finish[0];
+     reset();
+   }
+   if(operator_array[0] == "÷"){
+     finish.push(nums[0]/nums[1]);
+     display.innerText = finish[0];
+     reset();
+   }
+  } 
 }
 
-
+function reset() {
+  array.length=0;
+  num_array.length=0;
+  nums.length=0;
+  operator_array.length=0;
+  negative_num.length = 0
+}
   
 
