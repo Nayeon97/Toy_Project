ede7d1c4d17cc4c895ce4ce07677bc89051bf84b
Nayeon097 <naaay097@gmail.com>
2022-02-20 17:34:59 +0900

    음수와 음수 계산 오류 ,연산시 부동소수점 오류 수정

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,135 +1,93 @@
 'use strict'
 
-const result = document.getElementById('end').innerHTML; // =
-const decimal_point = document.getElementById('num_decimal').innerHTML; // 소수점
+// const result = document.getElementById('end').innerHTML; // =
+// const decimal_point = document.getElementById('num_decimal').innerHTML; // 소수점
 const display = document.getElementsByClassName('now_result_text')[0]; // 현재 계산
 const before_result = document.getElementsByClassName('before_result_text')[0]; // 이전 계산
-const number = document.getElementsByClassName('num'); // 0-9까지 숫자
-const oper = document.getElementsByClassName('operator'); // 연산
+// const number = document.getElementsByClassName('num'); // 0-9까지 숫자
+// const oper = document.getElementsByClassName('operator'); // 연산
 const btnAll = document.querySelectorAll('button');
+
+let num1 = []; // 첫번째 숫자
+let operitem = []; // 연산기호
+let num2 = []; // 두번째 숫자
+let calculate_result = []; // 계산결과
 
 btnAll.forEach(button => {
   button.addEventListener('click', (btn) => {
     switch(btn.target.className || btn.target.id){
-      case "num": // 숫자
-        if(finish.length==1){
-          before_result.innerText = `Ans = ${finish[0]}`;
-          display.innerText=null;
-          finish.length = 0;
+      case "num":
+      case "num_decimal":
+        if(calculate_result.length == 1){
+          display.innerText =null;
+          display.innerText += btn.target.textContent;
+          before_result.innerText = `Ans: ${calculate_result[0]}`;
+          calculate_result.length = 0;
+          num1.push(btn.target.textContent);
+          
         }
-        display.innerText += btn.target.textContent;
+        else if(calculate_result.length == 0 && operitem.length == 0){ // 사용자가 기호를 입력했는 경우에는 두번째 숫자.
+          display.innerText += btn.target.textContent;
+          num1.push(btn.target.textContent);
+        }
+        else if(calculate_result.length == 0 && operitem.length == 1){
+          display.innerText += btn.target.textContent;
+          num2.push(btn.target.textContent);
+        }
         break;
       case "operator": // 연산자
-        if(finish.length==1){
-          finish.length = 0;
-        }
         display.innerText += btn.target.textContent;
+        if(calculate_result.length==1){ // 만약 계산결과값이 있는 상태에서 기호 누르면
+          num1 = Number(calculate_result[0]); // 결과값이 num1 값이 된다. 
+          calculate_result.length = 0; // finish 초기화
+          operitem.push(btn.target.textContent);
+        }
+        else{
+          operitem.push(btn.target.textContent);
+          num1 = Number(num1.join('')); 
+          // 연산자를 누르면 -> num1에 저장해놓은 값들을 하나로 합쳐서 문자열로 만든다음 -> 숫자로 변환한다.
+        }
         break;
+      case "end":
+          num2 = Number(num2.join('')); 
+          pressdown_end();
+          reset();
+          break;  
       case "clear": // 끝
         display.innerText = null;
         before_result.innerText = null;  
-        finish.length = 0;
-        reset();
+        calculate_result.length = 0;
         break;
-      case "end":
-          Calculate_array();
-          Calculate_result();
-          input_display();
-          break;
-      case "num_decimal":
-        display.innerText += btn.target.textContent;
-          break;       
     }
   })
 });
 
-let array = []; 
-let num_array = []; 
-let operator_array = [];
-let nums = [];
-let finish = [];
-let negative_num = [];
-
-function Calculate_array(){
-   for(let i=0; i<display.innerText.length; i++){
-      array.push(display.innerText[i]);
-   }
-   return array;
+function pressdown_end(){
+  if(operitem[0] == "+"){
+    calculate_result.push(num1 + num2);
+    display.innerText = calculate_result[0];
+    before_result.innerText = `${num1} + ${num2}`;
+  }
+  if(operitem[0] == "-"){
+    calculate_result.push(num1 - num2);
+   display.innerText = calculate_result[0];
+   before_result.innerText = `${num1} - ${num2}`;
+ }
+ if(operitem[0] == "×"){
+  calculate_result.push(parseFloat((num1 * num2).toFixed(5)));
+   display.innerText = calculate_result[0];
+   before_result.innerText = `${num1} × ${num2}`;
+ }
+ if(operitem[0] == "÷"){
+   calculate_result.push(parseFloat((num1 / num2).toFixed(5)));
+   display.innerText = calculate_result[0];
+   before_result.innerText = `${num1} ÷ ${num2}`;
+ }
 }
 
-function Calculate_result(){
-  array.forEach((item, index) => {
-    if (index == 0 && item == "-")
-    {
-      negative_num.push(item);
-    }
-    else{
-      if(isNaN(item) == false || item === ".")
-      {
-         num_array.push(item);
-         if(index == array.length-1){
-          nums.push(parseFloat(num_array.join('')));
-         }
-      } 
-      else
-      {
-        nums.push(parseFloat(num_array.join('')));
-        num_array=[];
-        operator_array.push(item);
-      }    
-    }
-  });
+function reset(){
+  num1 = [];
+  num2 = [];
+  operitem = [];
 }
-
-function input_display(){
-  if(negative_num.length == 1){
-    console.log(operator_array[0]);
-    if(operator_array[0] == "+"){
-      finish.push((-1 * nums[0])+nums[1]);
-      display.innerText = finish[0];
-      reset();
-    }
-    if(operator_array[0] == "-"){
-     finish.push((-1 * nums[0])-nums[1]);
-     display.innerText = finish[0];
-     reset();
-   }
-   if(operator_array[0] == "×"){
-     finish.push((-1 * nums[0])*nums[1]);
-     display.innerText = finish[0];
-     reset();
-   }
-  } 
-  else{
-    if(operator_array[0] == "+"){
-      finish.push(nums[0]+nums[1]);
-      display.innerText = finish[0];
-      reset();
-    }
-    if(operator_array[0] == "-"){
-     finish.push(nums[0]-nums[1]);
-     display.innerText = finish[0];
-     reset();
-   }
-   if(operator_array[0] == "×"){
-     finish.push(nums[0]*nums[1]);
-     display.innerText = finish[0];
-     reset();
-   }
-   if(operator_array[0] == "÷"){
-     finish.push(nums[0]/nums[1]);
-     display.innerText = finish[0];
-     reset();
-   }
-  } 
-}
-
-function reset() {
-  array.length=0;
-  num_array.length=0;
-  nums.length=0;
-  operator_array.length=0;
-  negative_num.length = 0
-}
-  
 
