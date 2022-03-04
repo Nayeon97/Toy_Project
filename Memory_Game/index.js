633fd1917a5bf00d5499596a4cbb58ae6dbe9892
Nayeon097 <naaay097@gmail.com>
2022-03-04 16:14:19 +0900

    사용자가 선택한 카드와 비교 true, false 반환. geme_state 상태 변경

--- Memory_Game/index.js
+++ Memory_Game/index.js
@@ -5,12 +5,15 @@ const $start = document.querySelector('.start_btn');
 const $start_container = document.querySelectorAll('.start_container')[0];
 let randomCardnum=[]; // 랜덤으로
 let selectCardnum=[]; // 사용자가 선택
+let gamestateCount = 2; // game_state에 있는 count를 3번씩 반복하기 위해!
 
 //game state
 const game_state = {
   score: 0,
-  count: 10
+  count: 2
 }; 
+
+
 
 function selectCard (count) {
    // count 갯수만큼 랜덤으로 번호 생성하여 array에 push
@@ -19,7 +22,7 @@ function selectCard (count) {
     setTimeout(()=>{
       let num =  Math.floor(Math.random()*15);
       randomCardnum.push(num);
-      $cards[num].classList.add('select'); 
+      $cards[num].classList.add('select');
       setTimeout(()=>{
         $cards[num].classList.remove('select');
       },800) 
@@ -29,21 +32,57 @@ function selectCard (count) {
 }
 
 function clickCard () {
-  $cards.forEach((target, index) => {
+  let length = randomCardnum.length;
+  let i=0;
+    $cards.forEach((target, index) => {
       target.addEventListener('click', () => {
-        selectCardnum.push(index);
-        console.log("Dfdfd");
+         let result = resultCardclick(i,index);
+         if(result == true){
+           console.log("일치함");
+           if (i+1 == length) {
+              console.log("마지막입니다.");
+              document.body.classList.add("sucess");
+              setTimeout(()=>{
+                document.body.classList.remove("sucess");
+              },500)
+              gameState();
+           }
+           i++;
+         }
+         else {
+           // finish -> restart
+         }
       })
-  }) 
+  })
 }
 
+// game_state count 상태관리
+function gameState() {
+   if (gamestateCount == 2){
+      game_state.count ++;
+   }
+}
+
+function resultCardclick(i,index){
+    if(randomCardnum[i] == index){
+      return true;
+    }
+    else{
+      return false;
+    }
+}
+  
+
 function start() {
-  selectCard(game_state.count);
-  setTimeout(() => {
+  $start.addEventListener('click', ()=> {
+    $start_container.style.display ="none";
+    selectCard(game_state.count);
+    setTimeout(() => {
     if(game_state.count == randomCardnum.length){
       clickCard();
     }
-  },1000*game_state.count+2);
+  },1000*game_state.count+3);
+  })
 } 
 
 start();
